99577e545bf2e0d2a03fa8da5115e515cd299828
Anastasiya <namirono@mail.ru>
2023-10-05 10:16:33 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-     int price = 2_567;
+        int price = 2_567;
 
-     int miles = price/20 ;
+        int miles = price / 20;
 
-     System.out.println ( "Бонус: " + miles);
+        System.out.println("Бонус: " + miles);
     }
 }
